464807097c5cca3fe5c9fa63287ffe5b4c23c918
Arina3433 <arinakazakova343@gmail.com>
2023-08-25 19:52:50 +0400

    random number

--- src/QuestionsRepository.java
+++ src/QuestionsRepository.java
@@ -1,6 +1,25 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class QuestionsRepository {
 
     private ArrayList<String> questionsList = new ArrayList<>();
+    private ArrayList<Integer> randomNumbers = new ArrayList<>();
+
+    public ArrayList<Integer> getRandomNumbersArray() {
+
+        for (int i = 0; i < 5; i++) {
+            Random randNumber = new Random();
+            int newNumber = randNumber.nextInt(questionsList.size());
+
+            if (!randomNumbers.contains((newNumber+ 1))) {
+                randomNumbers.add((newNumber + 1));
+            } else {
+                i--;
+            }
+        }
+        return randomNumbers;
+    }
+
+
 }
